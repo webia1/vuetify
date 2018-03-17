9a663f3f8de284ee1d632b6df0cc8005c004dd3e
Webia1 <erkan.buelbuel@gmail.com>
2018-03-17 21:21:36 +0100

    IE11 & Safari Support added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import store from './store';
 
 import 'vuetify/dist/vuetify.min.css';
+import 'babel-polyfill';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
