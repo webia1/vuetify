18334f0da29caf78f5ea440aa9e565b3851d049c
Webia1 <erkan.buelbuel@gmail.com>
2018-03-17 21:17:32 +0100

    docs added

---
+++ docs/index.md
@@ -0,0 +1,24 @@
+# Index
+## Tags
+
+```jsx
+v-app
+v-navigation-drawer
+v-toolbar
+v-toolbar-side-icon
+v-content
+v-container
+v-footer
+```
+
+## Attributes
+
+```jsx
+app
+clipped
+clipped-left
+dark
+fixed
+flat
+fluid
+```
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-<v-app>
-  <v-navigation-drawer fixed clipped app>Navigation Drawer</v-navigation-drawer>
-  <v-toolbar dark color="red darken-4" app fixed clipped-left scroll-off-screen>Vtoolbar</v-toolbar>
-  <v-content>
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
-  </v-content>
-  <v-footer></v-footer>
-</v-app>
+  <v-app>
+    <v-navigation-drawer fixed clipped app v-model="drawer">
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/women/85.jpg">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Maxima Musterfrau</v-list-tile-title>
+          </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      
+    </v-navigation-drawer>
+    <v-toolbar dark color="red darken-4" app fixed clipped-left scroll-off-screen>
+      <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer></v-footer>
+  </v-app>
 
 </template>
 
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+};
+</script>
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+
+<style lang="stylus">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
