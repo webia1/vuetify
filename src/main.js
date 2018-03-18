c57746c73b5a7dc267d3f5fff592b0f44953fc77
Webia1 <erkan.buelbuel@gmail.com>
2018-03-18 12:04:05 +0100

    js-color-pack

--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import router from './router';
 import store from './store';
 
 import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
 import 'babel-polyfill';
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken1, // #E53935
+    secondary: colors.red.lighten4, // #FFCDD2
+    accent: colors.indigo.base // #3F51B5
+  }
+});
 
 new Vue({
   router,
