601457d98d7c05d633c2cfad1853b8957a923903
Webia1 <erkan.buelbuel@gmail.com>
2018-03-18 10:30:11 +0100

    vuetify doc update

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,30 @@
       <v-toolbar flat class="transparent">
         <v-list class="pa-0">
           <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="https://randomuser.me/api/portraits/women/85.jpg">
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>Maxima Musterfrau</v-list-tile-title>
-          </v-list-tile-content>
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/women/85.jpg">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Maxima Musterfrau</v-list-tile-title>
+            </v-list-tile-content>
           </v-list-tile>
         </v-list>
       </v-toolbar>
-      
     </v-navigation-drawer>
-    <v-toolbar dark color="red darken-4" app fixed clipped-left scroll-off-screen>
+    <v-toolbar dark color="blue" app fixed clipped-left scroll-off-screen>
+      <v-toolbar-title>Toolbar-Title</v-toolbar-title>
       <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
     </v-toolbar>
     <v-content>
-      <v-container fluid>
+      <v-container>
         <router-view></router-view>
       </v-container>
     </v-content>
-    <v-footer></v-footer>
+    <v-footer>
+      <v-toolbar>
+        
+      </v-toolbar>
+    </v-footer>
   </v-app>
 
 </template>
