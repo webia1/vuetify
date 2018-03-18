d8f9dee1edfbb8d99994d4e5680bf8d561e84613
Webia1 <erkan.buelbuel@gmail.com>
2018-03-18 12:14:00 +0100

    Minifying Vuetify on Production

--- src/main.js
+++ src/main.js
@@ -8,10 +8,18 @@ import 'vuetify/dist/vuetify.min.css';
 import 'babel-polyfill';
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  options: {
+    minifyTheme: function() {
+      return process.env.NODE_ENV === 'production'
+        ? val.replace(/[\s|\r\n|\r|\n]/g, '')
+        : null;
+    },
+  },
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
