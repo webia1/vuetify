8eefa15066a34e33355f96b91aaaa98921b9c7fc
Webia1 <erkan.buelbuel@gmail.com>
2018-03-18 12:27:56 +0100

    Variables and kbd

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Content from './views/Content.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/content',
+      name: 'content',
+      component: Content
     }
   ]
 })
