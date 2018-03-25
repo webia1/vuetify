3859c7b2aba4402f65fbc1948752fa45cbdfe729
Webia1 <erkan.buelbuel@gmail.com>
2018-03-25 22:10:21 +0200

    simplified

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/store';
 
 import 'vuetify/dist/vuetify.min.css';
 import 'babel-polyfill';
